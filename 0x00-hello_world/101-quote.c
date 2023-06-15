8c4c9acae204c00bc66172f520e636f2e8bea6ea
e4ma-okafor <ifeomasandra13@gmail.com>
2023-06-15 16:33:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+putchar(ch);
 return (1);
 }
