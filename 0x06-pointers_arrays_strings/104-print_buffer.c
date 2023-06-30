c7918ea7e61cb87257fa79e113d21985743b2ac6
e4ma-okafor <ifeomasandra13@gmail.com>
2023-06-29 18:03:13 -0700

    A C function that prints a buffer.

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to be printed
+ * @size: size of buffer
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, k, l;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%.8x:", i);
+			for (k = i; k < i + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = i; l < i + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
+	}
+}
