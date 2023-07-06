210e2606b6a6a396443f0b40ddac50df4bb93c42
e4ma-okafor <ifeomasandra13@gmail.com>
2023-07-05 20:29:20 -0700

    A C function that returns the value of x raised to the power of y.

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x raised to the
+ * power of y
+ * @x: the number to be computed
+ * @y: the exponential
+ * Return: the value of x raised to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
