63e44a2f28ad2c0d79398f5c91ff4ea450a99583
e4ma-okafor <ifeomasandra13@gmail.com>
2023-09-20 17:59:12 -0700

    dynamic lib

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * mul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * sub - subtracts two integers and returns
+ * the result.
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: Subtract number one from number two.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+ * mod - modulus of two integers and returns
+ * the result.
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: modulus operation of number one and number two.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
